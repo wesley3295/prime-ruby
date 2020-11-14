bd68948988e11972be663184576a3cb86fb3aa6a
Wesley <wspencer3295@yahoo.com>
2020-11-14 01:26:03 -0700

    commit tree 82133d06289b6aad21213d7dfd3904af376e9720

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+ #Add  code here!
+
+
+
+def prime?(num)
+    if  num < 2
+         return false
+          elsif
+            (2..num - 1).each do |asd|
+            if  num % asd == 0
+             return false
+            end
+         
+        end
+                return true
+    end
+end
